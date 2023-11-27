f42c4e0ddab4ea3daa1e02ac10f055330880bbfe
Charles E. Gormley <ceg98@drexel.edu>
2023-11-27 00:10:53 -0500

    first commit

---
+++ test/user_request_encoding_lambda_docker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UserRequestEncodingLambdaDocker from '../lib/user_request_encoding_lambda_docker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/user_request_encoding_lambda_docker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UserRequestEncodingLambdaDocker.UserRequestEncodingLambdaDockerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
